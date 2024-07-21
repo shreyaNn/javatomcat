3f8539e945eca49bef9e2dab380b4bce0ca79f35
shreyaNn <36974031+shreyaNn@users.noreply.github.com>
2024-07-21 21:23:46 +0530

    Update main.tf

--- Infra/main.tf
+++ Infra/main.tf
@@ -2,6 +2,17 @@ provider "aws" {
   region = var.region
 }
 
+# Get the default VPC
+data "aws_vpc" "default" {
+  default = true
+}
+
+# Get the default subnet
+data "aws_subnet_ids" "default" {
+  vpc_id = data.aws_vpc.default.id
+}
+
+# Get the default AMI for Amazon Linux
 data "aws_ami" "amazon_linux" {
   most_recent = true
   owners      = ["amazon"]
@@ -16,10 +27,11 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-resource "aws_security_group" "private_sg" {
-  name_prefix = "private-sg"
+# Create a security group in the default VPC
+resource "aws_security_group" "default_sg" {
+  name_prefix = "default-sg"
   description = "Allow inbound SSH traffic"
-  vpc_id      = data.terraform_remote_state.vpc.outputs.vpc_id
+  vpc_id      = data.aws_vpc.default.id
 
   ingress {
     from_port   = 22
@@ -36,12 +48,13 @@ resource "aws_security_group" "private_sg" {
   }
 }
 
-resource "aws_instance" "private" {
+# Create an EC2 instance in the default VPC
+resource "aws_instance" "default" {
   ami                   = data.aws_ami.amazon_linux.id
   instance_type         = var.instance_type
-  subnet_id             = element(data.terraform_remote_state.vpc.outputs.private_subnet_ids, 0)  # First private subnet
-  vpc_security_group_ids = [aws_security_group.private_sg.id]
+  subnet_id             = element(data.aws_subnet_ids.default.ids, 0)  # Use the first default subnet
+  vpc_security_group_ids = [aws_security_group.default_sg.id]
   tags = {
-    Name = "private-ec2"
+    Name = "default-ec2-instance"
   }
 }
