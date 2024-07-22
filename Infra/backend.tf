e5ca485923d87bbb07f5171e52cd7d9aabb89429
shreyaNn <36974031+shreyaNn@users.noreply.github.com>
2024-07-22 09:24:07 +0530

    Update backend.tf

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "demo1-s3bucket-terraform-tfstate"
-    key            = "test/ec2/terraform.tfstate"
+    key            = "prod/ec2/terraform.tfstate"
     region         = "ap-south-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
